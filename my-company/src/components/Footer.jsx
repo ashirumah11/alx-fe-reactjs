74819c8770027f815ac18b932ae12d846fbc62ad
ashirumah11 <josephanyangu1@gmail.com>
2025-11-08 14:00:58 +0300

    Adding my first React website MY COMPANY

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#eee', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
