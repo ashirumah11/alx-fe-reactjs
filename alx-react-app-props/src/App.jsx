ca19929b9af635b176ad4368ea145ee5023e241d
ashirumah11 <josephanyangu1@gmail.com>
2025-11-08 13:32:45 +0300

    Refactor prop driling using context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,16 @@
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
