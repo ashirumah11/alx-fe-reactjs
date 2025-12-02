4522aa2186b28953f1b280ce728989a573c7be78
ashirumah11 <josephanyangu1@gmail.com>
2025-12-02 17:50:42 +0300

    Building the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
       </h1>
 
       {/* Grid layout */}
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 sm:grid-cols-1 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
